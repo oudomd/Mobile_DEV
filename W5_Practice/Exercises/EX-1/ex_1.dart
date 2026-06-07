9eed3ebce0043793bf54ed04497c0635c30a1a3e
oudomd <oudomd369@proton.me>
2026-06-07 21:28:01 +0700

    Update ex_1.dart

--- W5_Practice/Exercises/EX-1/ex_1.dart
+++ W5_Practice/Exercises/EX-1/ex_1.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text("Custom buttons"),
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: const Text("Custom buttons")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ChangeButton(),
+            ChangeButton(),
+            ChangeButton(),
+            ChangeButton(),
+          ],
+        ),
+      ),
     ),
-    body: Center(
+  ),
+);
+
+class ChangeButton extends StatefulWidget {
+  @override
+  State<ChangeButton> createState() => _ChangeButtonState();
+}
+
+class _ChangeButtonState extends State<ChangeButton> {
+  bool isSelected = false;
+
+  String get label => isSelected ? 'Selected' : 'Not Selected';
+  Color get bgColor => isSelected ? Colors.blue[500]! : Colors.blue[50]!;
+  Color get textColor => isSelected ? Colors.white : Colors.black;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
       child: SizedBox(
         width: 400,
         height: 100,
         child: ElevatedButton(
-          onPressed: () => {},
-          child: const Center(
-            child: Text("Not Selected"),
-          )),
+          onPressed: () => setState(() => isSelected = !isSelected),
+          style: ElevatedButton.styleFrom(backgroundColor: bgColor),
+          child: Center(
+            child: Text(
+              label,
+              style: TextStyle(color: textColor, fontSize: 18),
+            ),
+          ),
+        ),
       ),
-    ),
-  ),
-));
+    );
+  }
+}
+
+
